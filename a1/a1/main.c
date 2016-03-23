bdbb6d422fa08996d15dbf3a704cca8184b11fee
isuwong <isuwong@126.com>
2016-03-23 19:58:07 +0800

    iOS

---
+++ a1/a1/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  a1
+//
+//  Created by lanou on 16/3/23.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int a;
+    printf("输入一个整数：");
+    scanf("%d",&a);
+    printf("%d",a);
+    
+    return 0;
+}
